cd7349b4aab6066c77a38b0cabb7e163f6bb6c2b
Henrik Enblom <henrik.enblom@enblom.com>
2025-04-06 13:19:07 +0200

    Reduce power consumption

--- src/main.cpp
+++ src/main.cpp
@@ -2,33 +2,27 @@
 #include "AudioTools.h"
 #include "AudioTools/AudioLibs/A2DPStream.h"
 
-AudioInfo info48k(44100, 1, 16);
 AudioInfo info44k1(44100, 2, 16);
 BluetoothA2DPSource a2dp_source;
 I2SStream i2s;
-FormatConverterStream conv(i2s);
-const int BYTES_PER_FRAME = 4;
 
+constexpr int BYTES_PER_FRAME = 4;
 
-int32_t get_sound_data(Frame *data, int32_t frameCount) {
-    return conv.readBytes((uint8_t *) data, frameCount * BYTES_PER_FRAME) / BYTES_PER_FRAME;
+int32_t get_sound_data(Frame *data, const int32_t frameCount) {
+    return static_cast<int32_t>(i2s.readBytes(reinterpret_cast<uint8_t *>(data), frameCount * BYTES_PER_FRAME)) /
+           BYTES_PER_FRAME;
 }
 
-// Arduino Setup
 void setup() {
-    setCpuFrequencyMhz(160);
+    setCpuFrequencyMhz(80);
     Serial.begin(115200);
     AudioToolsLogger.begin(Serial, AudioToolsLogLevel::Info);
-
-    // setup conversion
-    conv.setBuffered(false);
-    conv.begin(info48k, info44k1);
 
     // start i2s input with default configuration
     Serial.println("starting I2S...");
     auto cfg = i2s.defaultConfig(RX_MODE);
     cfg.i2s_format = I2S_STD_FORMAT; // or try with I2S_LSB_FORMAT
-    cfg.copyFrom(info48k);
+    cfg.copyFrom(info44k1);
     cfg.is_master = false;
     i2s.begin(cfg);
 
